5973e511859602224b995c9636fea048c8f1554a
HZenker <h.zenker@userart.net>
2019-10-15 08:46:25 +0200

    insert del

---
+++ PythonThreads.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# for 2'nd commit
+# in addition se README.md
+
+import sys, time, threading
+from concurrent import futures
+from threading import Condition
+import random
+
+#global Condition not necessarry, see main
+#condition = Condition()
+queue = []
+
+class Consumer(threading.Thread):
+    def __init__(self,name,conditi):
+        super().__init__()
+        self.condition = conditi
+        self.name = name
+        self.ti = time.localtime()
+    def run(self):
+        global queue
+        while True:
+            self.condition.acquire()
+            if not queue:
+                print ("Nothing in queue, consumer is waiting")
+                self.condition.wait()
+                print ("Producer added something to queue and notified the consumer")
+            num = queue.pop(0)
+            print ("Consumed", num)
+            self.condition.release()
+            #time.sleep(random.random())
+        
+
+class Producer(threading.Thread):
+    def __init__(self,name,conditi):
+        super().__init__()
+        self.condition = conditi
+        self.name = name
+        self.ti = time.localtime()
+        self.number = 0
+    def run(self):
+        global queue
+        while True:
+            self.condition.acquire()
+            self.number = self.number+1
+            #print("input int: ")
+            num =  self.number  #int(input())
+            queue.append(num)
+            print ("Produced", num)
+            self.condition.notify()
+            self.condition.release()
+            time.sleep(random.random())
+"""
+if __name__ =="__main__":
+    print(__name__) """
+cond = Condition()
+
+Consumer("consumer",cond).start()
+Producer("himan",cond).start()
+
+
---
+++ dealWithDB.py
@@ -0,0 +1,29 @@
+
+#! usr/bin/env python
+# -*- coding: utf-8 -*-
+#database test
+# in addition se README.md
+
+import sys, time, threading
+import sqlite3
+import mysql.connector
+
+class DealWithDB:
+    def __init__ (self,dbName):
+        self.dbName = dbName
+        self.dbConnect = ""
+        if self.dbName != "":
+            self.dbConnect = sqlite3.connect("dealDB01.db")
+    def __del__ (self):
+        self.dbName = ""
+    def getName(self):
+        print("method getName")
+        return self.dbName
+    def isConnected(self):
+        self.cursor = self.dbConnect.cursor()
+
+if __name__ =="__main__":
+    print(__name__)
+
+    db = DealWithDB("infoDB")
+    db.getName()
